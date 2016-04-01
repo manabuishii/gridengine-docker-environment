298e67cd81396f6a1fa7cec587d3aa68b71e50ca
Manabu ISHII <manabu.ishii.rb@gmail.com>
2016-04-01 18:12:07 +0900

    Bug fix wait job finish

--- result/qsubtest.sh
+++ result/qsubtest.sh
@@ -1,6 +1,17 @@
 qsub -j y -o /tmp/result/echo.log -wd /tmp/result /tmp/result/echo.sh
 qsub -j y -o /tmp/result/dockertest.log -wd /tmp/result /tmp/result/dockertest.sh
-sleep 10
+while true
+do
+  sleep 2
+  JOBS=$(qstat | wc -l)
+  if [ "${JOBS}" = "0" ]; then
+    break
+  else
+    RUNJOBS=$(($JOBS-2))
+    echo "${RUNJOBS} jobs is remained."
+  fi
+done
+
 RET=0
 grep "vagrant@exec" /tmp/result/echo.log
 RET=$((${RET}+$?))
